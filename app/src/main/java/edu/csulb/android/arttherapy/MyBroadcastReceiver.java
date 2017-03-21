9fdb8e618f2bf0d1acfde9b90f0edfebd09127d1
Hemant <nimje.hemant@gmail.com>
2017-03-20 17:03:12 -0700

    Add boardcast listener to show notification on screen unlock.

---
+++ app/src/main/java/edu/csulb/android/arttherapy/MyBroadcastReceiver.java
@@ -0,0 +1,52 @@
+package edu.csulb.android.arttherapy;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Hemant on 3/16/2017.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "MyBroadcastReceiver";
+
+    public MyBroadcastReceiver(){
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_touch_app_black_24dp)
+                .setContentTitle("My Notification")
+                .setContentText("Hello, World!");
+
+        Intent resultIntent = new Intent(context,MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        stackBuilder.addParentStack(MainActivity.class);
+
+        stackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        mBuilder.setFullScreenIntent(resultPendingIntent,false);
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(1,mBuilder.build());
+    }
+
+}
